8b59e1d5b79653c48ad28902b2664c6e6a711316
karasovden <karasovden@yandex.ru>
2018-12-24 01:29:18 +0300

    v2

--- avl_trees.php
+++ avl_trees.php
@@ -1,14 +1,68 @@
 <?php
 
-class avlTree
+class avlTreeNode
 {
     public $value;
-    public $lft = null;
-    public $rgt = null;
+    public $lft;
+    public $rgt;
 
     public function __construct($value)
     {
         $this->value = $value;
+        $this->lft = null;
+        $this->rgt = null;
+    }
+
+    public function dump()
+    {
+        if(!is_null($this->lft)){
+            $this->lft = dump();
+        }
+
+        if(!is_null($this->rgt)){
+            $this->rgt = dump();
+        }
+    }
+}
+
+class avlTree
+{
+    protected $root;
+
+    public function __construct()
+    {
+        $this->root = null;
+    }
+
+    public function insert($value)
+    {
+        $node = new avlTreeNode($value);
+
+        if(is_null($this->root)){
+            $this->root = $node;
+        } else {
+            $this->insertValue($node, $this->root);
+        }
+    }
+
+    public function insertValue($node, &$subtree)
+    {
+        if(is_null($subtree)){
+           $subtree = $node;
+        } else {
+            if($node->value > $subtree->value){
+                $this->insertValue($node, $subtree->rgt);
+            } else if($node->value < $subtree->value) {
+                $this->insertValue($node, $subtree->lft);
+            } else {
+                echo "все тлен";
+            }
+        }
+    }
+
+    public function observeTree()
+    {
+        $this->root->dump();
     }
 }
 
